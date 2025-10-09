70e71628e5471e326cba1d7100444bece1411526
unknown <umeshbhuvanchandratentu@gmail.com>
2025-10-09 23:08:22 +0530

    npm project commited

---
+++ Practice/loan-origination/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import UserDashboard from './components/UserDashboard/UserDashboard';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import UserDashboard from './components/UserDashBoard/UserDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <UserDashboard />
+    </div>
+  );
+}
+
+export default App;
